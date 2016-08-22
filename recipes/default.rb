a100cdd69fa52d00c72445e39adfef941f61a3cb
Annie Hedgpeth <annie.hedgpeth@gmail.com>
2016-08-22 10:10:49 -0500

    added attributes to default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,17 @@
 
 node.default['java']['jdk_version'] = '7'
 include_recipe 'java'
+
+node.default['elasticsearch']['version'] = '2.3.4'
+node.default['elasticsearch']['install_type'] = :package
+node.default['elasticsearch']['download_urls']['debian'] = 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-%s.deb'
+
+include_recipe 'chef-sugar'
+
+# see README.md and test/fixtures/cookbooks for more examples!
+elasticsearch_user 'elasticsearch'
+elasticsearch_install 'elasticsearch' do
+  type node['elasticsearch']['install_type'].to_sym # since TK can't symbol.
+end
+elasticsearch_configure 'elasticsearch'
+elasticsearch_service 'elasticsearch'
