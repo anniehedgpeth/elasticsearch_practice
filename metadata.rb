9ed39059a8b273e2b0e30da2993b55e540b2e9d3
Annie Hedgpeth <annie.hedgpeth@gmail.com>
2016-08-22 09:27:51 -0500

    added java

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,4 @@ long_description 'Installs/Configures elasticsearch_practice'
 version '0.1.0'
 
 depends 'elasticsearch'
+depends 'java'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,6 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+node.default['java']['jdk_version'] = '7'
+include_recipe 'java'
